2d04d923d927248c5b9c9428db439a29b2605c54
V4ldum <56064897+V4ldum@users.noreply.github.com>
2023-08-15 19:11:02 +0200

    types: added JsonResponse helper type

--- lib/types.dart
+++ lib/types.dart
@@ -3,3 +3,6 @@ export 'src/exceptions/option_exception.dart';
 export 'src/exceptions/result_exception.dart';
 export 'src/types/option.dart';
 export 'src/types/result.dart';
+
+/// Helper type for data in a Json format
+typedef JsonResponse = Map<String, dynamic>;
