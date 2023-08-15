742928600506465bac9ddd519ead9da7b01d2c58
V4ldum <56064897+V4ldum@users.noreply.github.com>
2023-08-15 19:33:06 +0200

    fix: added base_presenter to barrel file

--- lib/types.dart
+++ lib/types.dart
@@ -1,6 +1,7 @@
 export 'src/exceptions/displayable_exception.dart';
 export 'src/exceptions/option_exception.dart';
 export 'src/exceptions/result_exception.dart';
+export 'src/types/base_presenter.dart';
 export 'src/types/option.dart';
 export 'src/types/result.dart';
 
