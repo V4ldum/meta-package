976c1f03395caa0bc61fbb5307a8797ccbf405fa
V4ldum <56064897+V4ldum@users.noreply.github.com>
2023-08-15 19:35:38 +0200

    improvements: mountGuard now pass the context to the function

--- lib/src/types/base_presenter.dart
+++ lib/src/types/base_presenter.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 /// Base class for a Presenter with all the useful helper methods
 class BasePresenter {
   /// Executes the function if the context is currently mounted
-  void mountGuard(BuildContext context, VoidCallback func) {
+  void mountGuard(BuildContext context, void Function(BuildContext) func) {
     if (!context.mounted) {
       return;
     }
-    func();
+    func(context);
   }
 
   /// Display a snack bar
