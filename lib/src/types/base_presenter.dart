7c89d299e156d54aabfb1f2082615b27a598f0cd
V4ldum <56064897+V4ldum@users.noreply.github.com>
2023-08-15 19:32:07 +0200

    types: added a BasePresenter with helper methods

---
+++ lib/src/types/base_presenter.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+/// Base class for a Presenter with all the useful helper methods
+class BasePresenter {
+  /// Executes the function if the context is currently mounted
+  void mountGuard(BuildContext context, VoidCallback func) {
+    if (!context.mounted) {
+      return;
+    }
+    func();
+  }
+
+  /// Display a snack bar
+  void displaySnackBar({required BuildContext context, required Widget content}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: content),
+    );
+  }
+
+  /// Display a pop up
+  Future<void> displayPopUp({required BuildContext context, required AlertDialog dialog}) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return dialog;
+      },
+    );
+  }
+}
