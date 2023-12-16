410e092ed8b66701bc39436eb2c1f1bccbbf2152
V4ldum <56064897+V4ldum@users.noreply.github.com>
2023-12-16 22:35:11 +0100

    fix(base_controller): added const constructor to the base controller

--- lib/src/types/base_controller.dart
+++ lib/src/types/base_controller.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 /// Base class for a Presenter with all the useful helper methods
 class BaseController {
+  /// Default const constructor for the BaseController
+  const BaseController();
+
   /// Display a snack bar
   void displaySnackBar({required BuildContext context, required Widget content}) {
     ScaffoldMessenger.of(context).showSnackBar(
