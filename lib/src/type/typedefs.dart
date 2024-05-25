3c6aca555780c1242ba0ad12d810acf05630d0cd
V4ldum <56064897+V4ldum@users.noreply.github.com>
2024-05-25 17:03:39 +0200

    fix(typedefs): change JsonResponse to JsonMapResponse and add JsonListResponse

--- lib/src/type/typedefs.dart
+++ lib/src/type/typedefs.dart
@@ -1,2 +1,5 @@
-/// Helper type for data in a Json format
-typedef JsonResponse = Map<String, dynamic>;
+/// Helper type for data in a Json map format
+typedef JsonMapResponse = Map<String, dynamic>;
+
+/// Helper type for data in a Json list format
+typedef JsonListResponse = List<dynamic>;
